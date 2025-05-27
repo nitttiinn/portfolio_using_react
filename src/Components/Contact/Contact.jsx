64e1e09a092f09a1354a0425d47d535c782618c6
nittin <nittinsy12@gmail.com>
2025-05-27 23:09:21 +0530

    Added responsiveness to nav bar

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,40 +6,65 @@ import call_icon from '../../assets/call_icon.svg';
 import location_icon from '../../assets/location_icon.svg';
 
 const Contact = () => {
-  return (
-    <div className='contact'>
-        <div className="contact-title">
-            <h1> Get in Touch</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-        <div className="contact-section">
-            <div className="contact-left">
-                <h1>Let's Talk</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aliquid?</p>
-                <div className="contact-details">
-                    <div className="contact-detail">
-                        <img src={mail_icon} alt="Mail" /><p>nittinsy12@gmail.com</p>                  
-                    </div>
-                    <div className="contact-detail">
-                        <img src={call_icon} alt="PhoneNumber" /><p>+91123453210</p>
-                    </div>
-                    <div className="contact-detail">
-                        <img src={location_icon} alt="Location" /><p>Gurugram,India</p>
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const accesskey  = 'ea78e5b-92cc-4ce3-b8aa-f8f7ee62ac1f';
+        formData.append("access_key", accesskey); 
+        console.log("access_key", accesskey);
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Response:", res);
+            alert("Message sent successfully!");
+        }
+    };
+
+    return (
+        <div className='contact'>
+            <div className="contact-title">
+                <h1> Get in Touch</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div id='contact' className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's Talk</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aliquid?</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src={mail_icon} alt="Mail" /><p>nittinsy12@gmail.com</p>                  
+                        </div>
+                        <div className="contact-detail">
+                            <img src={call_icon} alt="PhoneNumber" /><p>+91123453210</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={location_icon} alt="Location" /><p>Gurugram,India</p>
+                        </div>
                     </div>
                 </div>
+                <form onSubmit={onSubmit} className="contact-right">
+                    <label htmlFor="">Your Name</label>
+                    <input type="text" placeholder='Enter your name' name='name'/>
+                    <label htmlFor="">Your Email</label>
+                    <input type="email" placeholder='Enter your email' name = 'email'/>
+                    <label htmlFor="">Message</label>
+                    <textarea name="message" rows="9" placeholder='Enter your message'></textarea>
+                    <button type='submit' className='contact-submit'>Send</button>
+                </form>
             </div>
-            <form className="contact-right">
-                <label htmlFor="">Your Name</label>
-                <input type="text" placeholder='Enter your name' name='name'/>
-                <label htmlFor="">Your Email</label>
-                <input type="email" placeholder='Enter your email' name = 'email'/>
-                <label htmlFor="">Message</label>
-                <textarea name="messaege" rows="9" placeholder='Enter your message'></textarea>
-                <button type='submit' className='contact-submit'>Send</button>
-            </form>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Hero.css';
 import profile_img from '../../assets/profile_img.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt="nittin" />
         <h1><span>I'm Nittin Singh Yadav, </span> Software Developer based in Gurgoan(India).</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis blanditiis maiores quis autem, dolores architecto nulla, quidem voluptate nemo ducimus ab? Ad, nam officiis!</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect With me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
         </div>
         
